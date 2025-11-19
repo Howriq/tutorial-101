683df75b7fa5a75904540d67926fb668f8f95f91
Howriq <horea@rospace.com>
2025-11-19 19:01:12 +0200

    Updates chapter-1

--- src/App/src/ConfigProvider.php
+++ src/App/src/ConfigProvider.php
@@ -112,14 +112,6 @@ class ConfigProvider
                     ],
                 ],
             ],
-            'connection'    => [
-                'orm_default' => [
-                    'doctrine_mapping_types' => [
-                        UuidBinaryType::NAME            => 'binary',
-                        UuidBinaryOrderedTimeType::NAME => 'binary',
-                    ],
-                ],
-            ],
             'driver'        => [
                 // The default metadata driver aggregates all other drivers into a single one.
                 // Override `orm_default` only if you know what you're doing.
@@ -137,8 +129,6 @@ class ConfigProvider
             ],
             'types'         => [
                 UuidType::NAME                  => UuidType::class,
-                UuidBinaryType::NAME            => UuidBinaryType::class,
-                UuidBinaryOrderedTimeType::NAME => UuidBinaryOrderedTimeType::class,
             ],
         ];
     }
